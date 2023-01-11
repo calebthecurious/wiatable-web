7652678e4a3675c75fcc65e3f963248063b85c9a
calebthecurious <calebthecurious@gmail.com>
2023-01-11 18:55:08 +1100

    2nd commit - Navbar Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import { useState } from "react";
 
+import Navbar from "@/scenes/navbar";
+
+enum SelectedPage {
+  Home = "home",
+  About = "about",
+  LearnLocally = "learnLocally",
+  ShareSkill = "shareSkill",
+  ContactUs = "contactUs",
+}
+
 function App() {
-  return <div className="app">app</div>;
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
 }
 
 export default App;
