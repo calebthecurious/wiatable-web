1035b026b676db1cfd1d89a32dafed4c1356f995
calebthecurious <calebthecurious@gmail.com>
2023-01-13 13:57:46 +1100

    3rd commit - Enums Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+import { SelectedPage } from "@/shared/types";
 import Navbar from "@/scenes/navbar";
-
-enum SelectedPage {
-  Home = "home",
-  About = "about",
-  LearnLocally = "learnLocally",
-  ShareSkill = "shareSkill",
-  ContactUs = "contactUs",
-}
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
     SelectedPage.Home
   );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div className="app bg-gray-20">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   );
 }
---
+++ src/shared/types.ts
@@ -0,0 +1,7 @@
+export enum SelectedPage {
+  Home = "home",
+  About = "about",
+  LearnLocally = "learnLocally",
+  ShareSkill = "shareSkill",
+  ContactUs = "contactUs",
+}
